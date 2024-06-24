b6faae2905778c7403763e26a3c734e15cc0eb41
TS_YESWANTH <tsyeswanth.eee2022@citchennai.net>
2024-06-24 22:31:14 +0530

    Video 7 Completed

---
+++ lib/features/authentication/screens/login/login.dart
@@ -0,0 +1,41 @@
+import 'package:e_commerce/common/styles/spacing_styles.dart';
+import 'package:e_commerce/common/widgets/login_signup/form_divider.dart';
+import 'package:e_commerce/common/widgets/login_signup/social_buttons.dart';
+import 'package:e_commerce/features/authentication/screens/login/widgets/login_form.dart';
+import 'package:e_commerce/features/authentication/screens/login/widgets/login_header.dart';
+import 'package:e_commerce/utils/contants/colors.dart';
+import 'package:e_commerce/utils/contants/image_strings.dart';
+import 'package:e_commerce/utils/contants/sizes.dart';
+import 'package:e_commerce/utils/contants/text_strings.dart';
+import 'package:e_commerce/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:iconsax/iconsax.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = THelperFunctions.isDarkMode(context);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: TSpacingStyle.paddingWithAppBarHeight,
+          child: Column(
+            children: [
+              const TLoginHeader(),
+              const TLoginForm(),
+              TFormDivider(dividerText: TTexts.orSignInWith.capitalize!),
+              const SizedBox(
+                height: TSizes.spaceBtwSections,
+              ),
+              const TSocialButtons()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
