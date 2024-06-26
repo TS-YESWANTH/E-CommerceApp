4d416a74007f19039949c569b070e65dae08d4dd
TS_YESWANTH <tsyeswanth.eee2022@citchennai.net>
2024-06-27 00:59:26 +0530

    Video 12 Completed

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/common/widgets/custom_shapes/containers/primary_header_constainer.dart';
 import 'package:flutter/material.dart';
 
 class THomeScreen extends StatelessWidget {
@@ -5,6 +6,12 @@ class THomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [TPrimaryHeaderContainer(child: Container())],
+        ),
+      ),
+    );
   }
 }
